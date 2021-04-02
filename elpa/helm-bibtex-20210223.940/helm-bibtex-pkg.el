f115fbfb16cd39945bedc6cc09d1eea4c38be530
Marco Hassan <marcohassan@Marcos-MBP.lan>
2021-04-02 17:28:36 +0200

    some new packages

---
+++ elpa/helm-bibtex-20210223.940/helm-bibtex-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from helm-bibtex.el  -*- no-byte-compile: t -*-
+(define-package "helm-bibtex" "20210223.940" "A bibliography manager based on Helm" '((bibtex-completion "1.0.0") (helm "1.5.5") (cl-lib "0.5") (emacs "24.1")) :authors '(("Titus von der Malsburg" . "malsburg@posteo.de")) :maintainer '("Titus von der Malsburg" . "malsburg@posteo.de") :url "https://github.com/tmalsburg/helm-bibtex")
